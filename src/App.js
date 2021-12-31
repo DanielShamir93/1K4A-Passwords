77e71cf61244fd9bb3728a871fae75e091d8cfc9
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-12-31 10:29:34 +0200

    Add AuthRoute component to redirect after user authenticated and trying to go to signup or login page without logout properly

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './styles/App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Signup from './pages/signup/components/Signup.component';
 import Login from './pages/login/components/Login.component';
 import Home from './pages/home/components/Home.component';
 import ProtectedRoute from './components/ProtectedRoute.component';
+import AuthRoute from './components/AuthRoute.component';
 
 function App() {
   return (
     <Router>
-        <Route path="/" exact component={Signup} />
-        <Route path="/login" exact component={Login} />
-        <ProtectedRoute path="/home" exact component={Home} />
-      </Router>
+      <AuthRoute path="/" redirectPath='/home' exact component={Signup} />
+      <AuthRoute path="/login" redirectPath="/home" component={Login} />
+      <ProtectedRoute path="/home" redirectPath="/" component={Home} />
+    </Router>
   );
 }
 
--- src/components/AuthRoute.component.jsx
+++ src/components/AuthRoute.component.jsx
@@ -1,7 +1,7 @@
 import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-export default function ProtectedRoute({ component: Component, redirectPath, ...rest }) {
+export default function LoginRoute({ component: Component, redirectPath, ...rest }) {
 
     const statesObject = useSelector((state) => {
         return {isAuth: state.isAuth}
@@ -12,9 +12,9 @@ export default function ProtectedRoute({ component: Component, redirectPath, ...
             {...rest}
             render={(props) => {
                 if (statesObject.isAuth) {
-                    return <Component />
-                } else {
                     return <Redirect to={ { pathname: redirectPath, state: {from: props.location}} } />
+                } else {
+                    return <Component />
                 }
             }
         }/>
