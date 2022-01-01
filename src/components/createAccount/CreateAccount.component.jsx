7e8d37d12402d2ff92acf4da9866d21f82a39322
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-01 10:43:47 +0200

    Add Password class

--- src/components/createAccount/CreateAccount.component.jsx
+++ src/components/createAccount/CreateAccount.component.jsx
@@ -1,37 +1,58 @@
 import "./create-account.styles.scss";
 import { useState } from "react";
 import { addDoc } from "firebase/firestore";
+import hash from "object-hash";
 
-export default function CreateAccount( { collectionRef } ) {
+import Password from "../../modules/Password";
 
-    const [name, setName] = useState("");
-    const [publicKey, setPublicKey] = useState("");
+export default function CreateAccount({ collectionRef }) {
+  const [name, setName] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
 
-    const createAccount = async () => {
-        try {
-            await addDoc(collectionRef, {name, publicKey});
-        } catch(err) {
-            console.log(err.massage);
-        }
+
+  const createAccount = async () => {
+    try {
+    //   console.log(privateKey);
+    //   const publicKey = Math.random();
+    //   const hashedPrivateKey = hash(privateKey);
+    //   const hashedPublicKey = hash(publicKey);
+    //   const password = hash({ hashedPrivateKey, hashedPublicKey });
+
+        const password = new Password(privateKey, 'wow');
+        password.setKeyboard({avoidChars: '', isIncludeDigits: true, isIncludeUpperCase: true, isIncludeLowerCase: true, isIncludeSymbols: true, mustIncludeChars: ''})
+        password.generate({PassLength: 10, PassStartsWith: '', PassEndsWidth: ''})
+        // console.log(password.get())
+
+    //   console.log(password)
+      // await addDoc(collectionRef, {name, publicKey: privateKey});
+    } catch (err) {
+      console.log(err.massage);
     }
+  };
 
-    return (
-        <div className="CreateAccount">
-            <form className="create-account-form">
-                <input 
-                    type="text" 
-                    placeholder="Name"
-                    onChange={(e) => {setName(e.target.value)}}
-                    value={name}
-                />
-                <input 
-                    type="text" 
-                    placeholder="Private Key"
-                    onChange={(e) => {setPublicKey(e.target.value)}}
-                    value={publicKey}
-                />
-                <button type="button" onClick={createAccount}>Submit</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="CreateAccount">
+      <form className="create-account-form">
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
+        />
+        <input
+          type="text"
+          placeholder="Private Key"
+          onChange={(e) => {
+            setPrivateKey(e.target.value);
+          }}
+          value={privateKey}
+        />
+        <button type="button" onClick={createAccount}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
---
+++ src/modules/Password.js
@@ -0,0 +1,64 @@
+import hash from "object-hash";
+
+// const keyboard = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "\"", "#", "$", "%", "&", "'", "\(", "\)", "*", "+", ",", "-", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\[", "\\", "\]", "^", "_", "`", "\{", "|", "\}", "~"];
+
+export default class Password {
+
+    constructor(privateKey, publicKey) {
+        this.privateKey = privateKey;
+        this.publicKey = publicKey;
+        this.password = "";
+        this.keyboard = [];
+    }
+
+    setKeyboard = ({ avoidChars, isIncludeDigits, isIncludeUpperCase, isIncludeLowerCase, isIncludeSymbols, mustIncludeChars }) => {
+
+        const symbols = "!\\#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+        for (let i = 33; i < 127; i++) {
+            let char = String.fromCharCode(i);
+
+            if (/[0-9]/.test(char) && !avoidChars.includes(char) && isIncludeDigits) {
+                this.keyboard.push(char);
+            } else if (/[A-Z]/.test(char) && !avoidChars.includes(char) && isIncludeUpperCase) {
+                this.keyboard.push(char);
+            } else if (/[a-z]/.test(char) && !avoidChars.includes(char) && isIncludeLowerCase) {
+                this.keyboard.push(char);
+            } else if (symbols.includes(char) && !avoidChars.includes(char) && isIncludeSymbols) {
+                this.keyboard.push(char);
+            }
+        }
+
+
+        Array.from(mustIncludeChars).forEach((char) => {
+            if (!this.keyboard.includes(char)) {
+                this.keyboard.push(char);
+            }
+        })
+
+    }
+
+    generate = ({ PassLength, PassStartsWith, PassEndsWidth }) => {
+        const passwordLength = PassLength - PassStartsWith.length - PassEndsWidth.length;
+        const hashedCombineKeys = hash({privateKey: this.privateKey, publicKey: this.publicKey});
+
+        const hashedCombinedKeysSum = Array.from(hashedCombineKeys).reduce((prevVal, currVal) => {
+            return prevVal + currVal.charCodeAt(0)}, 0);
+
+        this.password += PassStartsWith;
+
+        for (let i = 0; i < Math.min(hashedCombineKeys.length, passwordLength); i++) {
+            let keyBoardIndex = (i + hashedCombineKeys[i].charCodeAt(0) + hashedCombinedKeysSum) % this.keyboard.length;
+            this.password += this.keyboard[keyBoardIndex];
+        }
+
+        this.password += PassEndsWidth;
+        console.log(this.password)
+    }
+    
+
+    get = () => {
+        return this.password;
+    }
+}
+
