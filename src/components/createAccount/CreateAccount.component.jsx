b806434232cd271c1f3bba42d68b55f1eb95a23f
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-02 18:55:30 +0200

    Add docs to Firebase

--- src/components/createAccount/CreateAccount.component.jsx
+++ src/components/createAccount/CreateAccount.component.jsx
@@ -1,11 +1,10 @@
 import "./create-account.styles.scss";
-import { useState, usEffect } from "react";
+import { useState } from "react";
 import hash from "object-hash";
 import ToggleButtonsMultiple from "../toggleButtonsMultiple/ToggleButtonsMultiple.component";
 import Password from "../../modules/Password";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../../firebase/firebase-config";
-const accountsCollectionRef = collection(db, "accounts");
 
 export default function CreateAccount() {
   const [output, setOutput] = useState("");
@@ -14,10 +13,12 @@ export default function CreateAccount() {
   const [passLength, setPassLength] = useState(12);
   const [passStartsWith, setPassStartsWith] = useState("");
   const [passEndsWith, setPassEndsWith] = useState("");
-  const [passMustInclude, setPassMustInclude] = useState("");
-  const [avoidCharsInPass, setAvoidCharsInPass] = useState("");
+  const [passMustContain, setPassMustContain] = useState("");
+  const [passAvoidChars, setPassAvoidChars] = useState("");
   const [passPattern, setPassPattern] = useState("");
   const [privateKey, setPrivateKey] = useState("");
+  const [publicKey, setPublicKey] = useState("");
+  const [isValidAccount, setIsValidAccount] = useState(false);
   const [isChecked, setIsChecked] = useState({
     isDigitsChecked: true,
     isUppercaseChecked: true,
@@ -27,45 +28,67 @@ export default function CreateAccount() {
 
   const createAccount = async () => {
     try {
-      // const publicKey = hash(Math.random());
-      // const password = new Password(privateKey, publicKey);
-      // password.setKeyboard({avoidChars: avoidCharsInPass, isIncludeDigits: isDigitsChecked, isIncludeUpperCase: isUppercaseChecked, isIncludeLowerCase: isLowercaseChecked, isIncludeSymbols: isSymbolsChecked, mustIncludeChars: passMustInclude})
-      // password.generate({PassLength: PasswordLength, PassStartsWith: passStartsWith, PassEndsWidth: passEndsWith})
-      // console.log(password.get())
-      //   await addDoc(accountsCollectionRef, {name, publicKey: privateKey});
+      if (isValidAccount) {
+        const newAccount = {
+          accountName,
+          accountSubname,
+          passAvoidChars,
+          passEndsWith,
+          passLength,
+          passMustContain,
+          passPattern,
+          passStartsWith,
+          publicKey,
+          passHasDigit: isChecked.isDigitsChecked,
+          passHasUppercase: isChecked.isUppercaseChecked,
+          passHasLowercase: isChecked.isLowercaseChecked,
+          passHasSymbol: isChecked.isSymbolsChecked
+        }
+        await addDoc(collection(db, "accounts"), newAccount);
+        resetCreateAccountForm();
+      } 
     } catch (err) {
       console.log(err.massage);
     }
   };
+
+  const resetCreateAccountForm = () => {
+    
+  }
 
   const outputPassword = () => {
     if (accountName.length > 0) {
       if (privateKey.length > 0) {
         if (isChecked.isDigitsChecked || isChecked.isUppercaseChecked || isChecked.isLowercaseChecked || isChecked.isSymbolsChecked) {
           if (parseInt(passLength) > 0 && parseInt(passLength) < 41) {
-            const publicKey = hash(Math.random());
+            setIsValidAccount(true);
+            setPublicKey(hash(Math.random()));
             const password = new Password(privateKey, publicKey);
 
             if (passPattern.length > 0) {
-              password.setKeyboard({ avoidChars: avoidCharsInPass, isIncludeDigits: isChecked.isDigitsChecked, isIncludeUpperCase: isChecked.isUppercaseChecked, isIncludeLowerCase: isChecked.isLowercaseChecked, isIncludeSymbols: isChecked.isSymbolsChecked, mustIncludeChars: passMustInclude });
+              password.setKeyboard({ avoidChars: passAvoidChars, isIncludeDigits: isChecked.isDigitsChecked, isIncludeUpperCase: isChecked.isUppercaseChecked, isIncludeLowerCase: isChecked.isLowercaseChecked, isIncludeSymbols: isChecked.isSymbolsChecked, mustIncludeChars: passMustContain });
               password.generateFromPattern(passPattern);
               setOutput(password.getPassword);
             } else {
-              password.setKeyboard({ avoidChars: avoidCharsInPass, isIncludeDigits: isChecked.isDigitsChecked, isIncludeUpperCase: isChecked.isUppercaseChecked, isIncludeLowerCase: isChecked.isLowercaseChecked, isIncludeSymbols: isChecked.isSymbolsChecked, mustIncludeChars: passMustInclude });
+              password.setKeyboard({ avoidChars: passAvoidChars, isIncludeDigits: isChecked.isDigitsChecked, isIncludeUpperCase: isChecked.isUppercaseChecked, isIncludeLowerCase: isChecked.isLowercaseChecked, isIncludeSymbols: isChecked.isSymbolsChecked, mustIncludeChars: passMustContain });
               password.generate(passLength, passStartsWith, passEndsWith);
               setOutput(password.getPassword);
             }
           } else {
             setOutput("Password Length: Between 1 To 40");
+            setIsValidAccount(false);
           }
         } else {
-          setOutput("Missing Keyboard");
+          setOutput("Missing Characters In Keyboard");
+          setIsValidAccount(false);
         }
       } else {
         setOutput("Missing: Private Key");
+        setIsValidAccount(false);
       }
     } else {
       setOutput("Missing: Account Name");
+      setIsValidAccount(false);
     }
   }
 
@@ -80,7 +103,7 @@ export default function CreateAccount() {
       <form className="create-account-form">
         <div className="create-account-details">
           <fieldset className="account-details">
-            <legend>Account Settings:</legend>
+            <legend>Account Settings</legend>
             <div>
               <input
                 type="text"
@@ -103,7 +126,7 @@ export default function CreateAccount() {
             </div>
           </fieldset>
           <fieldset className="password-details">
-            <legend>Password Settings:</legend>
+            <legend>Password Settings</legend>
             <div>
               <ToggleButtonsMultiple
                 setCheckbox={setCheckbox}
@@ -147,9 +170,9 @@ export default function CreateAccount() {
                 type="text"
                 placeholder="Must Contain"
                 onChange={(e) => {
-                  setPassMustInclude(e.target.value);
+                  setPassMustContain(e.target.value);
                 }}
-                value={passMustInclude}
+                value={passMustContain}
               />
             </div>
             <div>
@@ -157,9 +180,9 @@ export default function CreateAccount() {
                 type="text"
                 placeholder="Avoid Characters"
                 onChange={(e) => {
-                  setAvoidCharsInPass(e.target.value);
+                  setPassAvoidChars(e.target.value);
                 }}
-                value={avoidCharsInPass}
+                value={passAvoidChars}
               />
             </div>
             <div>
@@ -194,7 +217,7 @@ export default function CreateAccount() {
             readOnly
           />
         </div>
-        <button className="submit-button" type="button" onClick={createAccount}>Submit</button>
+        <button className="submit-button" type="button" onClick={createAccount} disabled={!isValidAccount}>Submit</button>
       </form>
     </div>
   );
